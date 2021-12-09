d3d0e7c2cb756aa4e89507be7a368ca9a97479ab
Jan Galinski <jan.galinski@holisticon.de>
2021-12-09 19:08:04 +0100

    solve 5.2, add kotlin week 2

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -6,14 +6,35 @@ import io.toolisticon.lib.krid.Krids.krid
 import io.toolisticon.lib.krid.ascii
 import io.toolisticon.lib.krid.model.AddKrid
 import io.toolisticon.lib.krid.model.Cell
+import io.toolisticon.lib.krid.model.CellValue
+import io.toolisticon.lib.krid.model.step.CompositeStep
+import io.toolisticon.lib.krid.model.step.CoordinatesStep
+import io.toolisticon.lib.krid.model.step.DirectionStep
 import io.toolisticon.lib.krid.toAddKrid
 import kotlin.math.max
 
 fun main() {
-  val regex = """(\d+),(\d+) -> (\d+),(\d+)""".toRegex()
-  fun String.parseLine(): Pair<Cell, Cell> {
-    val (p1, p2, p3, p4) = regex.find(this)!!.destructured
-    return cell(p1.toInt(), p2.toInt()) to cell(p3.toInt(), p4.toInt())
+  fun List<Pair<Cell, Cell>>.emptyKrid(): Krid<Int> = map { max(it.first.x, it.second.x) to max(it.first.y, it.second.y) }
+    .fold(0 to 0) { (x, y), cur -> max(x, cur.first) to max(y, cur.second) }
+    .let {
+      krid(it.first + 1, it.second + 1, 0)
+    }
+
+  fun Pair<Cell, Cell>.vector(): Pair<Cell, DirectionStep> {
+    val move = cell(second.x - first.x, second.y - first.y)
+
+    val step = CompositeStep(move, true).single() as DirectionStep
+
+    return first to step
+  }
+
+  fun read(test: Boolean = false): List<Pair<Cell, Cell>> {
+    val regex = """(\d+),(\d+) -> (\d+),(\d+)""".toRegex()
+    fun String.parseLine(): Pair<Cell, Cell> {
+      val (p1, p2, p3, p4) = regex.find(this)!!.destructured
+      return cell(p1.trim().toInt(), p2.trim().toInt()) to cell(p3.trim().toInt(), p4.trim().toInt())
+    }
+    return readInput("Day05" + if (test) "_test" else "").map { it.parseLine() }
   }
 
   data class Venture(var krid: Krid<Int>) {
@@ -26,94 +47,20 @@ fun main() {
       krid = krid.plus(a)
     }
 
-    fun count() = krid.iterator().asSequence().map { it.value }.count { it > 1 }
+    fun count() = krid.cellValues().map { it.value }.count { it > 1 }
 
-    fun ascii() = krid.ascii { if (it == 0) '.' else if (it > 1) '*' else '1' }
-  }
-
-  fun List<Pair<Cell, Cell>>.emptyKrid(): Venture = map { max(it.first.x, it.second.x) to max(it.first.y, it.second.y) }
-    .fold(0 to 0) { (x, y), cur -> max(x, cur.first) to max(y, cur.second) }
-    .let {
-      Venture(krid(it.first + 1, it.second + 1, 0))
-    }
-
-  fun part2(moves: List<Pair<Cell, Cell>>): Int {
-    val k = moves.emptyKrid()
-    val p1 = mutableListOf<Pair<Cell, Cell>>()
-    val p2 = mutableListOf<Pair<Cell, Cell>>()
-    val p3 = mutableListOf<Pair<Cell, Cell>>()
-
-    moves.forEach {
-      if (it.first.x == it.second.x && it.first.y != it.second.y) {
-        p1.add(it)
-      } else if (it.first.y == it.second.y && it.first.x != it.second.x) {
-        p2.add(it)
-      } else {
-        p3.add(it)
+    fun ascii() = krid.ascii {
+      when {
+        it == 0 -> '.'
+        it > 1 -> '*'
+        else -> '1'
       }
     }
-
-    fun toAddKrid(p: Pair<Cell, Cell>, init: (Int, Int) -> Int): AddKrid<Int> {
-      val ps = if (p.first < p.second) {
-        p
-      } else {
-        p.second to p.first
-      }
-
-println(ps)
-      val k = krid(
-        width = 1 + ps.second.x - ps.first.x,
-        height = 1 + ps.second.y - ps.first.y,
-        emptyElement = 0,
-        initialize = init
-      )
-
-      return k.toAddKrid(offset = ps.first) { o, n -> o + n }
-    }
-    println("p3: $p3")
-
-    val a1 = p1.map { toAddKrid(it) { _, _ -> 1 } }
-    val a2 = p2.map { toAddKrid(it) { _, _ -> 1 } }
-    val a3 = p3.map { toAddKrid(it) { x,y-> if (x==y) 1 else 0 } }
-
-
-    println("p1: $a1")
-    println("p2: $a2")
-    println("p3: $p3")
-
-//    val cols: List<Pair<Cell, Krid<Int>>> = moves.filter { it.first.x == it.second.x }.map { if (it.first.y < it.second.y) it else it.second to it.first }
-//      .map {
-//        it.first to krid(
-//          width = 1,
-//          height = it.second.y - it.first.y + 1,
-//          emptyElement = 0
-//        ) { _, _ -> 1 }
-//      }
-//
-//    val rows = moves.filter { it.first.y == it.second.y }.map { if (it.first.x < it.second.x) it else it.second to it.first }
-//      .map { it.first to krid(
-//        width = it.second.x - it.first.x + 1,
-//        height = 1,
-//        emptyElement = 0) { _, _ -> 1 }
-//      }
-//
-//    val diagonals = moves.filter { it.first.y != it.second.y && it.first.x != it.second.x }.map { if (it.first > it.second) it else it.second to it.first }
-//      .map {
-//        it.first to krid(
-//          width = it.second.x - it.first.x + 1,
-//          height = it.second.y - it.first.y + 1,
-//          emptyElement = 0
-//        ) { x, y -> if (x == y) 1 else 0 }
-//      }
-
-    (a1 + a2).forEach { k.plus(it) }
-    println(k.ascii())
-
-    return k.count()
   }
+
 
   fun part1(moves: List<Pair<Cell, Cell>>): Int {
-    val k = moves.emptyKrid()
+    val k = Venture(moves.emptyKrid())
 
     val cols = moves.filter { it.first.x == it.second.x }.map { if (it.first.y < it.second.y) it else it.second to it.first }
       .map { it.first to krid(width = 1, height = it.second.y - it.first.y + 1, emptyElement = 0) { _, _ -> 1 } }
@@ -123,17 +70,42 @@ println(ps)
 
     (cols + rows).forEach { k.plus(it) }
 
-    println(k.ascii())
-
     return k.count()
   }
 
-  val moves: List<Pair<Cell, Cell>> = readInput("Day05_test").map { it.parseLine() }
+  fun part2(moves: List<Pair<Cell, Cell>>): Int {
+    val vectors = moves.map { it.vector() }
+      // build beamss of cellValue(1)
+      .map { v ->
+        v.second.direction.beam(v.first, true)
+          .take(v.second.number + 1)
+          .map { CellValue(it, 1) }
+          .toList()
+      }
+      // build a krid that just contains the beams
+      .map {
+        moves.emptyKrid() + it
+      }
+      // reduce all krids by adding up cellValues
+      .fold(moves.emptyKrid()) { r, c ->
+        r + c.toAddKrid { o, n -> o + n }
+      }
+
+    // count all values > 1
+    return vectors.cellValues().count { it.value > 1 }
+  }
 
 
+  fun part22(moves: List<Pair<Cell, Cell>>): Int  = moves.map { it.vector() }
+      .flatMap {
+        it.second.direction.beam(it.first, true)
+          .take(it.second.number + 1)
+          .toList()
+      }.groupingBy { it }.eachCount().toList()
+      .count { it.second > 1 }
 
-  println(part2(moves))
-  //println("dimension: \n${moves.emptyKrid().ascii()}")
 
-
+  println(part22(read(true)))
+  println(part22(read(false))) // wrong
 }
+
---
+++ src/main/kotlin/week2/Tree.kt
@@ -0,0 +1,17 @@
+package io.github.jangalinski.kata.aoc2021.week2
+
+
+sealed class Tree<T> {
+  override fun toString(): String = when (this) {
+    is Leaf -> value.toString()
+    is Node -> "($left, $right)"
+  }
+}
+
+data class Leaf<T>(val value: T) : Tree<T>()
+data class Node<T>(val left: Tree<T>, val right: Tree<T>) : Tree<T>()
+
+fun Tree<*>.count(): Int = when (this) {
+  is Leaf -> 1
+  is Node -> left.count() + right.count()
+}
